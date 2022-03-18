2805fb1eabade5427c2bd306a04096dea7d4a007
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-03-18 13:45:23 +0000

    Add tests for the token service

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,8 +18,8 @@ def app():
     _app.config["PRESERVE_CONTEXT_ON_EXCEPTION"] = False
 
     # minimum require configuration for CognitoAuth extension
-    _app.config["AWS_COGNITO_USER_POOL_ID"] = "eu-west-1_123456"
-    _app.config["AWS_COGNITO_USER_POOL_CLIENT_ID"] = "random-client-id"
+    _app.config["AWS_COGNITO_USER_POOL_ID"] = "eu-west-1_Drvd8r4TM"
+    _app.config["AWS_COGNITO_USER_POOL_CLIENT_ID"] = "545isk1een1lvilb9en643g3vd"
     _app.config["AWS_COGNITO_USER_POOL_CLIENT_SECRET"] = "secure-client-secret"
     _app.config["AWS_COGNITO_REDIRECT_URL"] = "http://example.com/redirect"
     _app.config["AWS_COGNITO_DOMAIN"] = "http://auth.example.com"
@@ -35,3 +35,35 @@ def app():
 def client():
     cl = app.test_client()
     yield cl
+
+
+@pytest.fixture
+def jwks():
+    return {
+        "keys": [
+            {
+                "alg": "RS256",
+                "e": "AQAB",
+                "kid": "2hQZJREoWZ/3A7hEYG+iIa7GJD+Jweu1caQ4rFrh2JM=",
+                "kty": "RSA",
+                "n": "m_FrYse7laSfIvHgKHVJzRknFnEjad79b0hrqQ1FoNOZ_JX5_15lSnHy0gPM542ZZ_cjCe6tbEavz4dI3g0CxZRW6esjXzRefVAuphilpQ1gmQDjASa6Qg2LqUS1Hd04m9UGSJo9vdG1KRsOK-MXGaV5EglKaTcINcVs31-B5R53rjuwTEcWpMlYb9VRq86VUdGEzH4I74sa6NYo3dSftL9N0ghH2lq0I2l2taVCH7FUk3phOeksNyTQgxnWQ-pGYzqpZOcZmEEdQMT3fjd4_pcqXSYrB3lmSN0nXxorq1RGmkRRQ3d70-Veyh4KvU-f_VzIdwIc5yLOzf3RaHMvFw",
+                "use": "sig",
+            },
+            {
+                "alg": "RS256",
+                "e": "AQAB",
+                "kid": "pv5k2Fdq+5uVgcb4jrgA76H7iVGvAO4uOmhpCheqTDo=",
+                "kty": "RSA",
+                "n": "pFToxHflSw-b8kfjaTERryoHdI4D1NaFCCNkNW1qaPSVp3FYZj4TzD3giF-XrnL0YgW_EpLs02mFWqHexgYfN-vJNOvbreT0wsmnzBoK2SlSKWqh70OBF26eVmmNCqMfRdNoP2QcqcagoKFRUkaxhC4TdVzPzb7l-xOnXrqsQlKCsR7ULuxYzBoRbSDJSq2YosE228Fq8ysMScle5i07fFUjpqnL3Yw1GQ3FPuBHYu5McAqLe1d_rRg2ER0FjVSggFut-3XICfe8Km8MCqglmnNT60RZo-ibsEXmN8zu2sJumyGLkGEDHJOf1VwKIdABWIey7UTlI2eYlqZRET04nw",
+                "use": "sig",
+            },
+        ]
+    }
+
+
+@pytest.fixture
+def jwks_endpoint_request(mocker, jwks):
+    response = mocker.Mock()
+    response.json = mocker.Mock(return_value=jwks)
+    request = mocker.Mock(return_value=response)
+    return request
