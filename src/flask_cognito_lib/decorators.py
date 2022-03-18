4ab6a7e1c2882ab0bdfc5890dfa000984c734c67
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-03-18 15:58:22 +0000

    First implementation of decorators
    Will add groups support later

--- src/flask_cognito_lib/decorators.py
+++ src/flask_cognito_lib/decorators.py
@@ -1,8 +1,16 @@
+from base64 import urlsafe_b64encode
 from functools import wraps
-from typing import List
+from hashlib import sha256
+from os import urandom
+from typing import Dict
 
-from flask import current_app, redirect, request
-from flask_jwt_extended import set_access_cookies, unset_jwt_cookies
+from flask import current_app, jsonify, redirect, request, session
+from flask_jwt_extended import (
+    get_jwt_identity,
+    set_access_cookies,
+    unset_jwt_cookies,
+    verify_jwt_in_request,
+)
 from werkzeug.local import LocalProxy
 
 from flask_cognito_lib.config import Config
@@ -11,12 +19,42 @@ cfg = Config()
 _auth_cls = LocalProxy(lambda: current_app.extensions[cfg.APP_EXTENSION_KEY])
 
 
+def secure_random(n_bytes: int = 16) -> str:
+    """Generate a secure URL-safe random string"""
+    return urlsafe_b64encode(urandom(n_bytes)).decode("utf-8")
+
+
+def update_session(state: Dict[str, str]):
+    """Update the Flask session with key/value pairs"""
+    session.update(state)
+
+
 def cognito_login(fn):
     """A decorator that redirects to the Cognito hosted UI"""
 
     @wraps(fn)
     def wrapper(*args, **kwargs):
-        res = redirect(cfg.login_endpoint)
+        # store parameters in the session that are passed to Cognito
+        # and required for JWT verification
+        code_verifier = secure_random()
+        cognito_session = {
+            "code_verifier": code_verifier,
+            "code_challenge": sha256(code_verifier),
+            "nonce": secure_random(),
+            "state": secure_random(),
+        }
+        update_session(cognito_session)
+
+        # TODO add support for scopes
+        res = redirect(
+            _auth_cls.cognito_service.get_sign_in_url(
+                code_verifier=cognito_session["code_verifier"],
+                code_challenge=cognito_session["code_challenge"],
+                nonce=cognito_session["nonce"],
+                state=cognito_session["state"],
+            )
+        )
+
         return res
 
     return wrapper
@@ -30,7 +68,31 @@ def cognito_login_callback(fn):
 
     @wraps(fn)
     def wrapper(*args, **kwargs):
-        access_token = _auth_cls.get_access_token(request.args)
+        # Get the access token return after auth flow with Cognito
+        code_verifier = session["code_verifier"]
+        state = session["state"]
+        code_challenge = session["code_challenge"]
+        nonce = session["nonce"]
+
+        # exchange the code for an access token
+        access_token = _auth_cls.get_access_token(
+            request_args=request.args,
+            expected_state=state,
+            code_verifier=code_verifier,
+        )
+
+        # validate the JWT
+        claims = _auth_cls.decode_and_verify_token(
+            token=access_token,
+            code_verifier=code_verifier,
+            code_challenge=code_challenge,
+            nonce=nonce,
+            state=state,
+        )
+
+        update_session({"claims": claims})
+
+        # return and set the JWT as a http only cookie
         resp = fn(*args, **kwargs)
         set_access_cookies(resp, access_token, max_age=30 * 60)  # TODO config
         return resp
@@ -43,6 +105,18 @@ def cognito_logout(fn):
 
     @wraps(fn)
     def wrapper(*args, **kwargs):
+        # remove the congito vars from the session
+        update_session(
+            {
+                "code_verifier": None,
+                "code_challenge": None,
+                "nonce": None,
+                "state": None,
+                "claims": None,
+            }
+        )
+
+        # logout at cognito and remove the cookies
         res = redirect(cfg.logout_endpoint)
         unset_jwt_cookies(res)
         return res
@@ -50,35 +124,36 @@ def cognito_logout(fn):
     return wrapper
 
 
-def auth_required(fn):
-    """View decorator that requires a valid Cognito JWT cookie."""
+def auth_required():
+    """A decorator to protect a route with AWS Cognito"""
 
-    @wraps(fn)
-    def decorator(*args, **kwargs):
-        _check_auth()
-        return fn(*args, **kwargs)
+    def wrapper(fn):
+        @wraps(fn)
+        def decorator(*args, **kwargs):
+            verify_jwt_in_request(optional=True)
+            if get_jwt_identity():
+                return fn(*args, **kwargs)
+            else:
+                return jsonify("Not authorised"), 403
 
-    return decorator
+        return decorator
 
-
-def groups_required(groups: List[str]):
-    """View decorator that requires a the user to in one or more Cognito groups."""
-
-    def decorator(function):
-        @wraps(function)
-        def wrapper(*args, **kwargs):
-            _check_groups(groups)
-            return function(*args, **kwargs)
-
-        return wrapper
-
-    return decorator
+    return wrapper
 
 
-def _check_auth():
-    # TODO
-    pass
+# TODO implement these
+# def groups_required(groups: List[str]):
+#     """View decorator that requires a the user to in one or more Cognito groups."""
 
+#     def decorator(function):
+#         @wraps(function)
+#         def wrapper(*args, **kwargs):
+#             _check_groups(groups)
+#             return function(*args, **kwargs)
 
-def _check_groups(groups: List[str]):
-    pass
+#         return wrapper
+
+#     return decorator
+
+# def _check_groups(groups: List[str]):
+#     pass
