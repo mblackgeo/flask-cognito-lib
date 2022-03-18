43d8713096abe62cdead5bd397b1d2b68024edc7
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-03-18 15:05:43 +0000

    Implement login/logout decorators

--- src/flask_cognito_lib/decorators.py
+++ src/flask_cognito_lib/decorators.py
@@ -1,7 +1,8 @@
 from functools import wraps
 from typing import List
 
-from flask import current_app
+from flask import current_app, redirect, request
+from flask_jwt_extended import set_access_cookies, unset_jwt_cookies
 from werkzeug.local import LocalProxy
 
 from flask_cognito_lib.config import Config
@@ -10,14 +11,43 @@ cfg = Config()
 _auth_cls = LocalProxy(lambda: current_app.extensions[cfg.APP_EXTENSION_KEY])
 
 
-def cognito_login_handler():
-    # TODO a handler that will redirect to the Cognito Hosted UI
-    pass
+def cognito_login(fn):
+    """A decorator that redirects to the Cognito hosted UI"""
+
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        res = redirect(cfg.login_endpoint)
+        return res
+
+    return wrapper
 
 
-def cognito_login_callback():
-    # TODO set a HTTPOnly cookie after login
-    pass
+def cognito_login_callback(fn):
+    """
+    A decorator to wrap the redirect after a user has logged in with Cognito.
+    Stores the Cognito JWT in a http only cookie.
+    """
+
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        access_token = _auth_cls.get_access_token(request.args)
+        resp = fn(*args, **kwargs)
+        set_access_cookies(resp, access_token, max_age=30 * 60)  # TODO config
+        return resp
+
+    return wrapper
+
+
+def cognito_logout(fn):
+    """A decorator that handles logging out with Cognito"""
+
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        res = redirect(cfg.logout_endpoint)
+        unset_jwt_cookies(res)
+        return res
+
+    return wrapper
 
 
 def auth_required(fn):
