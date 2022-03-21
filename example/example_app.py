60d16413c227d84ea87cc48486327ab886cf32d5
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-03-21 16:29:02 +0000

    Update the example application routes

--- example/example_app.py
+++ example/example_app.py
@@ -1,5 +1,5 @@
 from config import Config
-from flask import Flask, jsonify, session
+from flask import Flask, jsonify, redirect, session, url_for
 
 from flask_cognito_lib import CognitoAuth
 from flask_cognito_lib.decorators import (
@@ -8,6 +8,7 @@ from flask_cognito_lib.decorators import (
     cognito_login_callback,
     cognito_logout,
 )
+from flask_cognito_lib.exceptions import AuthorisationRequiredError
 
 app = Flask(__name__)
 
@@ -17,27 +18,62 @@ auth = CognitoAuth(app)
 
 
 @app.route("/")
+def home():
+    return "Hello world!"
+
+
+@app.route("/login")
 @cognito_login
 def login():
+    # No logic is required as the decorator ensure a redirect to the Cognito
+    # hosted UI for the user to sign in
     pass
 
 
 @app.route("/postlogin")
 @cognito_login_callback
 def postlogin():
-    pass
+    # A route to handle the redirect after a user has logged in with Cognito.
+    # The decorator will store the validated access token in a HTTP only cookie
+    # and the user claims are stored in the Flask session (session["claims"]).
+    # This route must be set as one of the User Pool client's Callback URLs.
+    # Do anything login after the user has logged in here, e.g. a redirect
+    return redirect(url_for("claims"))
 
 
 @app.route("/claims")
 @auth_required()
 def claims():
-    return jsonify({"claims": session["claims"]})
+    # This route is protected by the Cognito authorisation. If the user is not
+    # logged in at this point or their token from Cognito is no longer valid
+    # a 401 Authentication Error is thrown, which is caught here a redirected
+    # to login.
+    # If their session is valid, the claims from the Cognito JWT will be shown
+    return jsonify(session)
+
+
+@app.errorhandler(AuthorisationRequiredError)
+def login_redirect_handler(err):
+    # Register an error handler if the user hits an "@auth_required" route
+    # but is not logged in to redirect them to the Cognito UI
+    return redirect(url_for("login"))
 
 
 @app.route("/logout")
 @cognito_logout
 def logout():
+    # Logout of the Cognito User pool and delete the cookies that were set
+    # on login.
+    # No logic is required here as it simply redirects to Cognito
     pass
+
+
+@app.route("/postlogout")
+def postlogout():
+    # This is the endpoint Cognito redirects to after a user has logged out,
+    # handle any logic here, like returning to the homepage.
+    # This route must be set as one of the User Pool client's Sign Out URLs.
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
