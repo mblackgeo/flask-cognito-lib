f449c3bf8dedd87130a077dfa530bdba2bb43ef4
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-03-21 16:19:24 +0000

    Add cookie age and logout endpoint config

--- src/flask_cognito_lib/config.py
+++ src/flask_cognito_lib/config.py
@@ -1,4 +1,5 @@
 from typing import Any, Optional
+from urllib.parse import quote
 
 from flask import current_app
 
@@ -46,6 +47,7 @@ class Config:
     APP_EXTENSION_KEY = "cognito_auth_lib"
     CONTEXT_KEY_COGNITO_SERVICE = "aws_cognito_service"
     CONTEXT_KEY_TOKEN_SERVICE = "aws_jwt_service"
+    COOKIE_NAME = "cognito_access_token"
 
     @property
     def user_pool_id(self) -> str:
@@ -64,12 +66,20 @@ class Config:
         return get("AWS_COGNITO_REDIRECT_URL", required=True)
 
     @property
+    def logout_redirect(self) -> str:
+        return get("AWS_COGNITO_LOGOUT_URL", required=True)
+
+    @property
     def domain(self) -> str:
         return get("AWS_COGNITO_DOMAIN", required=True)
 
     @property
     def region(self) -> str:
         return get("AWS_REGION", required=True)
+
+    @property
+    def max_cookie_age_seconds(self) -> int:
+        return int(get("AWS_COGNITO_COOKIE_AGE_SECONDS", required=False, default=1800))
 
     @property
     def issuer(self) -> str:
@@ -88,12 +98,12 @@ class Config:
         return f"{self.domain}/oauth2/authorize"
 
     @property
-    def login_endpoint(self) -> str:
-        return f"{self.domain}/oauth2/login"
-
-    @property
     def logout_endpoint(self) -> str:
-        return f"{self.domain}/oauth2/logout"
+        return (
+            f"{self.domain}/logout"
+            f"?client_id={self.user_pool_client_id}"
+            f"&logout_uri={quote(self.logout_redirect)}"
+        )
 
     @property
     def user_info_endpoint(self) -> str:
