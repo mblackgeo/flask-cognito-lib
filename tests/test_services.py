53bec3c72b967d11c955ba650709e4d087661352
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-03-22 16:25:00 +0000

    Add tests for the factory methods

---
+++ tests/test_services.py
@@ -0,0 +1,11 @@
+from flask_cognito_lib.services import cognito_service_factory, token_service_factory
+from flask_cognito_lib.services.cognito_svc import CognitoService
+from flask_cognito_lib.services.token_svc import TokenService
+
+
+def test_cognito_service_factory(cfg):
+    assert isinstance(cognito_service_factory(cfg), CognitoService)
+
+
+def test_token_service_factory(cfg):
+    assert isinstance(token_service_factory(cfg), TokenService)
