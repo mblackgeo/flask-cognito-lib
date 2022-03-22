5f1297e1124c0acb18d86ef989fae53f24af34e3
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-03-22 15:42:59 +0000

    Improve test coverage of config

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,12 @@
 import pytest
 
+from flask_cognito_lib.config import get
 from flask_cognito_lib.exceptions import ConfigurationError
+
+
+def test_get_default():
+    res = get("ASDF", required=False, default=1)
+    assert res == 1
 
 
 def test_missing_config(app, cfg):
@@ -15,3 +21,42 @@ def test_issuer(cfg):
     """Check if forms the issuer URL correctly"""
     expected = "https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_c7O90SNDF"
     assert cfg.issuer == expected
+
+
+def test_client_secret(cfg):
+    assert cfg.user_pool_client_secret == "secure-client-secret"
+
+
+def test_logout_url(cfg):
+    assert cfg.logout_redirect == "http://localhost:5000/postlogout"
+
+
+def test_cookie_age(cfg):
+    # this is not set in the config so should be the default value
+    assert cfg.max_cookie_age_seconds == 1800
+
+
+def test_token_endpoint(cfg):
+    assert cfg.token_endpoint == (
+        "https://webapp-test.auth.eu-west-1.amazoncognito.com/oauth2/token"
+    )
+
+
+def test_logout_endpoint(cfg):
+    assert cfg.logout_endpoint == (
+        "https://webapp-test.auth.eu-west-1.amazoncognito.com/logout"
+        "?client_id=4lln66726pp3f4gi1krj0sta9h"
+        "&logout_uri=http%3A//localhost%3A5000/postlogout"
+    )
+
+
+def test_user_info_endpoint(cfg):
+    assert cfg.user_info_endpoint == (
+        "https://webapp-test.auth.eu-west-1.amazoncognito.com/oauth2/userInfo"
+    )
+
+
+def test_revoke_endpoint(cfg):
+    assert cfg.revoke_endpoint == (
+        "https://webapp-test.auth.eu-west-1.amazoncognito.com/oauth2/revoke"
+    )
