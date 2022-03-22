5483ca05c541ee253c18a4e1a40943f2c10b46fb
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-03-22 14:02:25 +0000

    Add nonce to assertion

--- tests/test_cognito_svc.py
+++ tests/test_cognito_svc.py
@@ -11,6 +11,7 @@ def test_sign_in_url(cfg):
     res = cognito.get_sign_in_url(
         code_challenge="asdf",
         state="1234",
+        nonce="6789",
         scopes=["openid", "profile"],
     )
     assert res == (
@@ -19,6 +20,7 @@ def test_sign_in_url(cfg):
         "&client_id=545isk1een1lvilb9en643g3vd"
         "&redirect_uri=http%3A//example.com/redirect"
         "&state=1234"
+        "&nonce=6789"
         "&code_challenge=asdf"
         "&code_challenge_method=S256"
         "&scopes=openid+profile"
