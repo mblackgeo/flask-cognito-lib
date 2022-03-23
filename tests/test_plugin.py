eda357e92774bd9b4423ee84d8645be08506bb1a
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-03-23 12:50:33 +0000

    Add test coverage for the plugin itself

---
+++ tests/test_plugin.py
@@ -0,0 +1,38 @@
+import pytest
+from flask import Flask
+
+from flask_cognito_lib import CognitoAuth
+from flask_cognito_lib.exceptions import CognitoError
+
+
+def test_plugin_init(cfg):
+    app = Flask(__name__)
+    CognitoAuth(app)
+    assert cfg.APP_EXTENSION_KEY in app.extensions
+
+
+def test_plugin_lazy_init(cfg):
+    app = Flask(__name__)
+    CognitoAuth().init_app(app)
+    assert cfg.APP_EXTENSION_KEY in app.extensions
+
+
+def test_plugin_get_tokens_parameters_missing(app, cfg):
+    cls = app.extensions[cfg.APP_EXTENSION_KEY]
+    with pytest.raises(CognitoError):
+        cls.get_tokens(
+            request_args={"code": "asdf"}, expected_state="", code_verifier=""
+        )
+        cls.get_tokens(
+            request_args={"state": "asdf"}, expected_state="", code_verifier=""
+        )
+
+
+def test_plugin_get_tokens_state_invalid(app, cfg):
+    cls = app.extensions[cfg.APP_EXTENSION_KEY]
+    with pytest.raises(CognitoError):
+        cls.get_tokens(
+            request_args={"code": "asdf", "state": "qwer"},
+            expected_state="1234",
+            code_verifier="",
+        )
