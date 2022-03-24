aa0a6e69847122428a457349b027a9d8c3498a58
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-03-24 10:00:57 +0000

    Rename example app, integrate config directly

--- example/app.py
+++ example/app.py
@@ -1,4 +1,6 @@
-from config import Config
+from os import environ, path, urandom
+
+from dotenv import load_dotenv
 from flask import Flask, jsonify, redirect, session, url_for
 
 from flask_cognito_lib import CognitoAuth
@@ -13,10 +15,32 @@ from flask_cognito_lib.exceptions import (
     CognitoGroupRequiredError,
 )
 
+# Load variables from .env
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, ".env"))
+
+
+class Config:
+    """Set Flask configuration vars from .env file."""
+
+    # General Config
+    SECRET_KEY = environ.get("SECRET_KEY", urandom(32))
+    FLASK_APP = "TEST_APP"
+    FLASK_ENV = "TESTING"
+
+    # Cognito config
+    AWS_REGION = environ["AWS_REGION"]
+    AWS_COGNITO_USER_POOL_ID = environ["AWS_COGNITO_USER_POOL_ID"]
+    AWS_COGNITO_DOMAIN = environ["AWS_COGNITO_DOMAIN"]
+    AWS_COGNITO_USER_POOL_CLIENT_ID = environ["AWS_COGNITO_USER_POOL_CLIENT_ID"]
+    AWS_COGNITO_USER_POOL_CLIENT_SECRET = environ["AWS_COGNITO_USER_POOL_CLIENT_SECRET"]
+    AWS_COGNITO_REDIRECT_URL = environ["AWS_COGNITO_REDIRECT_URL"]
+    AWS_COGNITO_LOGOUT_URL = environ["AWS_COGNITO_LOGOUT_URL"]
+    AWS_COGNITO_COOKIE_AGE_SECONDS = environ["AWS_COGNITO_COOKIE_AGE_SECONDS"]
+
+
 app = Flask(__name__)
-
 app.config.from_object(Config)
-
 auth = CognitoAuth(app)
 
 
@@ -28,8 +52,9 @@ def home():
 @app.route("/login")
 @cognito_login
 def login():
-    # No logic is required as the decorator ensure a redirect to the Cognito
-    # hosted UI for the user to sign in
+    # A simple route that will redirect to the Cognito Hosted UI.
+    # No logic is required as the decorator handles the redirect to the Cognito
+    # hosted UI for the user to sign in.
     pass
 
 
@@ -37,13 +62,12 @@ def login():
 @cognito_login_callback
 def postlogin():
     # A route to handle the redirect after a user has logged in with Cognito.
+    # This route must be set as one of the User Pool client's Callback URLs in
+    # the Cognito console and also as the config value AWS_COGNITO_REDIRECT_URL.
     # The decorator will store the validated access token in a HTTP only cookie
-    # and the user claims are stored in the Flask session (session["claims"]).
-    # This route must be set as one of the User Pool client's Callback URLs.
+    # and the user claims and info are stored in the Flask session:
+    # session["claims"] and session["user_info"].
     # Do anything login after the user has logged in here, e.g. a redirect
-
-    # TODO
-    # Custom redirect using the state value
     return redirect(url_for("claims"))
 
 
@@ -54,7 +78,8 @@ def claims():
     # logged in at this point or their token from Cognito is no longer valid
     # a 401 Authentication Error is thrown, which is caught here a redirected
     # to login.
-    # If their session is valid, the claims from the Cognito JWT will be shown
+    # If their session is valid, the current session will be shown including
+    # their claims and user_info extracted from the Cognito tokens.
     return jsonify(session)
 
 
