9c77c0f1a51a3880392c27aaa893ff5972f0ed43
Ryan Caught <rcaught@gmail.com>
2024-12-30 19:32:32 +1000

    fix: function name spelling
    Aliased and non-breaking.

--- src/flask_cognito_lib/plugin.py
+++ src/flask_cognito_lib/plugin.py
@@ -149,7 +149,7 @@ class CognitoAuth:
             If the request to the TOKEN endpoint fails
             If the TOKEN endpoint returns an error code
         """
-        return self.cognito_service.exhange_refresh_token(
+        return self.cognito_service.exchange_refresh_token(
             refresh_token=refresh_token,
         )
 
